fce3d709f8c121efe43471e7cc53321a3dc9d5f6
PyCrow <pythoncryptflower@gmail.com>
2023-06-06 20:59:28 +0300

    #17 ChannelData class defined

--- static_vars.py
+++ static_vars.py
@@ -10,6 +10,12 @@ KEY_MAX_DOWNLOADS = 'max_downloads'
 KEY_SCANNER_SLEEP = 'scanner_sleep'
 KEY_CHANNELS = 'channels'
 
+KEY_CHANNEL_NAME = 'name'
+KEY_CHANNEL_ALIAS = 'alias'
+KEY_CHANNEL_SVQ = 'svq'
+
+RECORD_QUALITY_TEMPLATE = 'bv*[height<={video}]+ba/b[height<={on_failed}]'
+
 DEFAULT_MAX_DOWNLOADS = 2
 DEFAULT_SCANNER_SLEEP = 300
 
@@ -20,6 +26,39 @@ RECORDS_PATH = CURRENT_PATH.joinpath('records')
 STYLESHEET_PATH = CURRENT_PATH.joinpath('ui').joinpath('stylesheet.qss')
 
 FLAG_LIVE = 'live event will begin in '
+
+
+class ChannelData:
+    """
+    Channel data
+     - name
+     - alias (editable)
+     - svq (stream video quality)
+    """
+    def __init__(self, name: str):
+        """
+        :param name: channel YouTube ID
+        (https://www.youtube.com/@channel_id).
+        Channel GUI alias can be changed in channel settings.
+        """
+        self.name: str = name
+        self.alias: str = name
+        self.svq: str = "1080"
+
+    def stream_quality(self) -> str:
+        return RECORD_QUALITY_TEMPLATE.format(
+            video=self.svq, on_failed=self.svq)
+
+    def jdump(self) -> dict:
+        return {
+            KEY_CHANNEL_NAME: self.name,
+            KEY_CHANNEL_ALIAS: self.alias,
+            KEY_CHANNEL_SVQ: self.svq,
+        }
+    def jload(self, data: dict):
+        self.name = data[KEY_CHANNEL_NAME]
+        self.alias = data[KEY_CHANNEL_ALIAS]
+        self.svq = data[KEY_CHANNEL_SVQ]
 
 
 class StopThreads(Exception):
